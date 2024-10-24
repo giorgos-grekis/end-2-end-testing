dbda9f7868790520baac6e44b0e8998051e53183
giorgos-grekis <georgrekis@gmail.com>
2024-10-24 13:10:41 +0300

    Selecting Elements & Evaluating Test Results

--- cypress/cypress_end_to_end_testing_getting_started/section_02/cypress/e2e/basic.cy.js
+++ cypress/cypress_end_to_end_testing_getting_started/section_02/cypress/e2e/basic.cy.js
@@ -1,7 +1,12 @@
+/// <reference types="Cypress" />
+
 describe("tasks page", () => {
   it("should render the main image", () => {
     // "visit" is not just a command that gets us to a page,
     // it also ensures that this page renders correctly and the test fails if it does not.
     cy.visit("http://localhost:5173/");
+
+    // use the "get" command to get something from the page
+    cy.get("img");
   });
 });
