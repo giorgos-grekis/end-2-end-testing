a7593e376259a66cb120237de20a52bba6a3abb2
giorgos-grekis <georgrekis.github@gmail.com>
2024-10-24 15:47:48 +0300

    working with values & aliases

--- cypress/cypress_end_to_end_testing_getting_started/section_03/cypress/e2e/contact.cy.js
+++ cypress/cypress_end_to_end_testing_getting_started/section_03/cypress/e2e/contact.cy.js
@@ -1,16 +1,31 @@
 describe("contact form", () => {
   it("should submit the form", () => {
+    // cy.visit("http://localhost:5173/about");
+    // cy.get('[data-cy="contact-input-message"]').type("Hello World");
+    // cy.get('[data-cy="contact-input-name"]').type("John Doe");
+    // cy.get('[data-cy="contact-input-email"]').type("test@test.com");
+    // cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
+    // cy.get('[data-cy="contact-btn-submit"]').should("not.have.attr", "disabled");
+
+    // cy.get('[data-cy="contact-btn-submit"]').click();
+
+    // // check if the button is disable
+    // cy.get('[data-cy="contact-btn-submit"]').contains("Sending...");
+    // cy.get('[data-cy="contact-btn-submit"]').should("have.attr", "disabled");
+
     cy.visit("http://localhost:5173/about");
     cy.get('[data-cy="contact-input-message"]').type("Hello World");
     cy.get('[data-cy="contact-input-name"]').type("John Doe");
     cy.get('[data-cy="contact-input-email"]').type("test@test.com");
-    cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
-    cy.get('[data-cy="contact-btn-submit"]').should("not.have.attr", "disabled");
+    cy.get('[data-cy="contact-btn-submit"]').contains("Send Message").and("not.have.attr", "disabled");
 
     cy.get('[data-cy="contact-btn-submit"]').click();
 
+    // const btn = cy.get('[data-cy="contact-btn-submit"]');
+    // recommended way is to store the value into the alias
+    cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
+
     // check if the button is disable
-    cy.get('[data-cy="contact-btn-submit"]').contains("Sending...");
-    cy.get('[data-cy="contact-btn-submit"]').should("have.attr", "disabled");
+    cy.get("@submitBtn").contains("Sending...").should("have.attr", "disabled");
   });
 });
