d8cabde1a8160a05e5c91d574148fc36c8643a8e
giorgos-grekis <georgrekis@gmail.com>
2024-10-24 14:07:11 +0300

    practice some test

--- cypress/cypress_end_to_end_testing_getting_started/section_02/cypress/e2e/tasks.cy.js
+++ cypress/cypress_end_to_end_testing_getting_started/section_02/cypress/e2e/tasks.cy.js
@@ -3,10 +3,15 @@
 describe("tasks management", () => {
   it("should open and close the new task modal", () => {
     cy.visit("http://localhost:5173/");
-    cy.contains("Add Task").click();
 
+    cy.contains("Add Task").click();
     // when the cypress click an element by default picks the center of the item.
     cy.get(".backdrop").click({ force: true });
+    cy.get(".backdrop").should("not.exist");
+    cy.get(".modal").should("not.exist");
+
+    cy.contains("Add Task").click();
+    cy.contains("Cancel").click();
     cy.get(".backdrop").should("not.exist");
     cy.get(".modal").should("not.exist");
   });
