edabf915ba73df8e52c3f013be6b6cb4f7d6e98d
giorgos-grekis <georgrekis@gmail.com>
2024-10-24 13:03:42 +0300

    Visiting page inside e23 tests

--- cypress/cypress_end_to_end_testing_getting_started/section_02/cypress/e2e/basic.cy.js
+++ cypress/cypress_end_to_end_testing_getting_started/section_02/cypress/e2e/basic.cy.js
@@ -1,5 +1,7 @@
 describe("tasks page", () => {
   it("should render the main image", () => {
+    // "visit" is not just a command that gets us to a page,
+    // it also ensures that this page renders correctly and the test fails if it does not.
     cy.visit("http://localhost:5173/");
   });
 });
