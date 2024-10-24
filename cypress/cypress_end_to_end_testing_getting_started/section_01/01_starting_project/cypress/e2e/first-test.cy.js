ba710f04ee9b18d3c40b7d839b7ca1cecf2dd1a2
giorgos-grekis <georgrekis@gmail.com>
2024-10-24 12:42:04 +0300

    Writing A More Realistic Test

--- cypress/cypress_end_to_end_testing_getting_started/section_01/01_starting_project/cypress/e2e/first-test.cy.js
+++ cypress/cypress_end_to_end_testing_getting_started/section_01/01_starting_project/cypress/e2e/first-test.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:5173/");
+
+    // check if we have 6 <li> elements
+    cy.get("li").should("have.length", 6);
+  });
+});
