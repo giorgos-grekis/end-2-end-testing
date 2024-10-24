e293dc7b4b874cb4a446296f4d24c8109226022b
giorgos-grekis <georgrekis.github@gmail.com>
2024-10-24 15:29:53 +0300

    testing page navigation

---
+++ cypress/cypress_end_to_end_testing_getting_started/section_03/cypress/e2e/navigation.cy.js
@@ -0,0 +1,13 @@
+describe("page navigation", () => {
+  it("should navigate between pages", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get('[data-cy="header-about-link"]').click();
+    cy.location("pathname").should("eq", "/about"); // /about => about page
+
+    cy.go("back");
+    cy.location("pathname").should("eq", "/"); // / => Home Page
+    cy.get('[data-cy="header-about-link"]').click();
+    cy.get('[data-cy="header-home-link"]').click();
+    cy.location("pathname").should("eq", "/"); // / => Home Page
+  });
+});
