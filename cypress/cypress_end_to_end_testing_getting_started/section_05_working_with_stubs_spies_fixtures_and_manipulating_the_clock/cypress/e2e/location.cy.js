17f79d103cb3ee687fbadeb6a257f623a763a611
giorgos-grekis <georgrekis.github@gmail.com>
2024-10-25 18:51:36 +0300

    Creating & Using a Stub

--- cypress/cypress_end_to_end_testing_getting_started/section_05_working_with_stubs_spies_fixtures_and_manipulating_the_clock/cypress/e2e/location.cy.js
+++ cypress/cypress_end_to_end_testing_getting_started/section_05_working_with_stubs_spies_fixtures_and_manipulating_the_clock/cypress/e2e/location.cy.js
@@ -1,8 +1,17 @@
 /// <reference types="cypress" />
 
-describe('share location', () => {
-  it('should fetch the user location', () => {
-    cy.visit('/');
+describe("share location", () => {
+  it("should fetch the user location", () => {
+    // This is how then works when used after visit.
+    // Visit yields the window object of the site that's visited just as get yields the element that we got when using then().
+    cy.visit("/").then((win) => {
+      // stub: takes two arguments
+      // the first argument points at the object that contains the method you want to replace (window.navigator.geolocation)
+      // the second argument is the name if the method you want to replace as a string
+      cy.stub(win.navigator.geolocation, "getCurrentPosition").as("getUserPosition");
+    });
+
     cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get("@getUserPosition").should("have.been.called");
   });
 });
