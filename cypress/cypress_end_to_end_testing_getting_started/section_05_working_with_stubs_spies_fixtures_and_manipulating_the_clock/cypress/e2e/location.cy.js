b9df87bd8e907cab7830afc68fa622101a728402
giorgos-grekis <georgrekis.github@gmail.com>
2024-10-25 19:20:35 +0300

    Using Stubs Across Tests

--- cypress/cypress_end_to_end_testing_getting_started/section_05_working_with_stubs_spies_fixtures_and_manipulating_the_clock/cypress/e2e/location.cy.js
+++ cypress/cypress_end_to_end_testing_getting_started/section_05_working_with_stubs_spies_fixtures_and_manipulating_the_clock/cypress/e2e/location.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe("share location", () => {
-  it("should fetch the user location", () => {
+  beforeEach(() => {
     // This is how then works when used after visit.
     // Visit yields the window object of the site that's visited just as get yields the element that we got when using then().
     cy.visit("/").then((win) => {
@@ -21,12 +21,19 @@ describe("share location", () => {
           }, 100);
         });
     });
+  });
 
+  it("should fetch the user location", () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get("@getUserPosition").should("have.been.called");
     // the button is disabled
     cy.get('[data-cy="get-loc-btn"]').should("be.disabled");
 
     cy.get('[data-cy="actions"]').should("contain", "Location fetched!");
+  });
+
+  it("should share a location URL", () => {
+    cy.get('[data-cy="name-input"]').type("John Doe");
+    cy.get('[data-cy="get-loc-btn"]').click();
   });
 });
