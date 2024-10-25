ea6be48934f1117b41180111caff63d7e4f10b2b
giorgos-grekis <georgrekis.github@gmail.com>
2024-10-25 22:09:24 +0300

    Accessing Interceptor Request & Response Data

--- cypress/cypress_end_to_end_testing_getting_started/section_06_network_hhtp_requests_databses_and_authentication/cypress/e2e/takeaways.cy.js
+++ cypress/cypress_end_to_end_testing_getting_started/section_06_network_hhtp_requests_databses_and_authentication/cypress/e2e/takeaways.cy.js
@@ -10,4 +10,21 @@ describe("Takeaways", () => {
 
     cy.get('[data-cy="takeaway-item"]').should("have.length", 2);
   });
+
+  it("should add a new takeaway", () => {
+    cy.intercept("POST", "/takeaways/new*", "success").as("createTakeaway");
+
+    cy.login();
+
+    cy.visit("/takeaways/new");
+
+    cy.get('[data-cy="title"]').click();
+    cy.get('[data-cy="title"]').type("TestTitle1");
+    cy.get('[data-cy="body"]').type("TestBody1");
+    cy.get('[data-cy="create-takeaway"]').click();
+    // regex "/TestTitle1.*TestBody1/": looks for "TestTitle1" then any amount of characters after it, and then "TestBody1"
+    cy.wait("@createTakeaway")
+      .its("request.body")
+      .should("match", /TestTitle1.*TestBody1/);
+  });
 });
