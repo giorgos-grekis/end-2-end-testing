bafbdf4cf3aa0fe41bc00e0b8797706e8939d899
giorgos-grekis <georgrekis.github@gmail.com>
2024-10-25 19:43:20 +0300

    Using Fixtures

--- cypress/cypress_end_to_end_testing_getting_started/section_05_working_with_stubs_spies_fixtures_and_manipulating_the_clock/cypress/e2e/location.cy.js
+++ cypress/cypress_end_to_end_testing_getting_started/section_05_working_with_stubs_spies_fixtures_and_manipulating_the_clock/cypress/e2e/location.cy.js
@@ -2,24 +2,24 @@
 
 describe("share location", () => {
   beforeEach(() => {
+    cy.fixture("user-location.json").as("userLocation");
+
     // This is how then works when used after visit.
     // Visit yields the window object of the site that's visited just as get yields the element that we got when using then().
     cy.visit("/").then((win) => {
-      // stub: takes two arguments
-      // the first argument points at the object that contains the method you want to replace (window.navigator.geolocation)
-      // the second argument is the name if the method you want to replace as a string
-      cy.stub(win.navigator.geolocation, "getCurrentPosition")
-        .as("getUserPosition")
-        .callsFake((cb) => {
-          setTimeout(() => {
-            cb({
-              coords: {
-                latitude: 37.5,
-                longitude: 48.01,
-              },
-            });
-          }, 100);
-        });
+      cy.get("@userLocation").then((fakePosition) => {
+        // stub: takes two arguments
+        // the first argument points at the object that contains the method you want to replace (window.navigator.geolocation)
+        // the second argument is the name if the method you want to replace as a string
+        cy.stub(win.navigator.geolocation, "getCurrentPosition")
+          .as("getUserPosition")
+          .callsFake((cb) => {
+            setTimeout(() => {
+              cb(fakePosition);
+            }, 100);
+          });
+      });
+
       cy.stub(win.navigator.clipboard, "writeText").as("saveToClickBoard").resolves();
     });
   });
@@ -38,6 +38,9 @@ describe("share location", () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('[data-cy="share-loc-btn"]').click();
     cy.get("@saveToClickBoard").should("have.been.called");
-    cy.get("@saveToClickBoard").should("have.been.calledWithMatch", new RegExp(`${37.5}.*${48.01}.*${encodeURI("John Doe")}`));
+    cy.get("@userLocation").then((fakePosition) => {
+      const { latitude, longitude } = fakePosition.coords;
+      cy.get("@saveToClickBoard").should("have.been.calledWithMatch", new RegExp(`${latitude}.*${longitude}.*${encodeURI("John Doe")}`));
+    });
   });
 });
